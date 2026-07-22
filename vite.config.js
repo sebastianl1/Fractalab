ad836547f21a2dcd5ef89fad1c264cb001f3477a
naitsabes <sebasbele11@gmail.com>
2026-07-22 13:28:08 -0500

    Vfixes

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/bifurcation-mandelbrot-explorer/',
+  base: './',
   build: {
     outDir: 'docs',
     assetsDir: 'assets',
